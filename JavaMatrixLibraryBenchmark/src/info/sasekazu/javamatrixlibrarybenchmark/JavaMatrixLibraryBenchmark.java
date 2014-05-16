b7ef493a2340c46fdcf147d180052235e1796214
Kazuya Sase <sasekazu@gmail.com>
2014-05-16 20:40:44 +0900

    Initial upload

---
+++ JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
@@ -0,0 +1,13 @@
+package info.sasekazu.javamatrixlibrarybenchmark;
+
+public class JavaMatrixLibraryBenchmark {
+
+	public JavaMatrixLibraryBenchmark() {
+		System.out.println("test");
+	}
+
+	public static void main(String[] args) {
+		new JavaMatrixLibraryBenchmark();
+	}
+
+}
