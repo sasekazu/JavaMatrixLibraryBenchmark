5970028e81bd7f1329a66a50d34dbe92de31a079
Kazuya Sase <sasekazu@gmail.com>
2014-05-16 21:11:51 +0900

    Test time measuring

--- JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
+++ JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
@@ -4,6 +4,12 @@ public class JavaMatrixLibraryBenchmark {
 
 	public JavaMatrixLibraryBenchmark() {
 		System.out.println("test");
+        long start = System.currentTimeMillis();
+        for(int i=0; i<10000; i++){
+        	System.out.println(""+i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)  + "ms");
 	}
 
 	public static void main(String[] args) {
