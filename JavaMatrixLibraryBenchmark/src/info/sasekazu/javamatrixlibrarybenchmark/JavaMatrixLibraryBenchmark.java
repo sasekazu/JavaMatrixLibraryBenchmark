c3f94a168670c4d033d734dd15591ec1c813fe2e
Kazuya Sase <sasekazu@gmail.com>
2014-05-17 14:57:23 +0900

    Add Colt

--- JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
+++ JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
@@ -18,11 +18,12 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.la4j.LinearAlgebra;
 import org.la4j.factory.Basic1DFactory;
 import org.la4j.linear.LinearSystemSolver;
-import org.la4j.matrix.Matrix;
-import org.la4j.vector.Vector;
 
 public class JavaMatrixLibraryBenchmark extends JFrame {
 	private static final long serialVersionUID = 1L;
+	final int max = 1000;
+	final int min = 2;
+	final int bin = 20;
 
 	public static void main(String[] args) {
 		JavaMatrixLibraryBenchmark frame = new JavaMatrixLibraryBenchmark();
@@ -33,92 +34,131 @@ public class JavaMatrixLibraryBenchmark extends JFrame {
 	}
 
 	JavaMatrixLibraryBenchmark() {
+		XYSeries dataJblasDouble = runJblasDouble();
+		XYSeries dataJblasFloat = runJblasFloat();
+		XYSeries dataEjml = runEjml();
+		XYSeries dataLa4j = runLa4j();
+		XYSeries dataJama = runJama();
+		XYSeries dataColt = runColt();
 
-		final int n = 1000;
-		long seed = System.currentTimeMillis();
-		Random rand = new Random(seed);
-		
-		// jblas
-		XYSeries dataJblas = new XYSeries("jblas");
-		System.out.println("Matrix Solver Benchmark. Solve for X in AX = B");
-		for (int i = 2; i < n; i+=10) {
-			System.out.println("jblas i="+i);
+		// JFreeChartによるグラフの作成
+		XYSeriesCollection data = new XYSeriesCollection();
+		data.addSeries(dataJblasDouble);
+		data.addSeries(dataJblasFloat);
+		data.addSeries(dataEjml);
+		data.addSeries(dataLa4j);
+		data.addSeries(dataJama);
+		data.addSeries(dataColt);
+		JFreeChart chart = ChartFactory.createScatterPlot(
+				"Benchmark of linear solver", "Matrix size", "Time [ms]", data,
+				PlotOrientation.VERTICAL, true, false, false);
+		ChartPanel cpanel = new ChartPanel(chart);
+		getContentPane().add(cpanel, BorderLayout.CENTER);
+	}
+	
+	
+	XYSeries runJblasDouble(){
+		XYSeries data = new XYSeries("jblas(double)");
+		for (int i = min; i < max; i += bin) {
+			System.out.println("jblas(double) i=" + i);
 			org.jblas.DoubleMatrix A = org.jblas.DoubleMatrix.rand(i, i);
 			org.jblas.DoubleMatrix b = org.jblas.DoubleMatrix.rand(i);
 			long s = System.nanoTime();
 			org.jblas.Solve.solve(A, b);
 			long e = System.nanoTime();
-			dataJblas.add(i, (e - s)*0.000001);
+			data.add(i, (e - s) * 0.000001);
 		}
-		
-		// EJML
-		// float型マトリクスがないみたい
-		// ベクトル型はないみたい
+		return data;
+	}
+	
+	XYSeries runJblasFloat(){
+		XYSeries data = new XYSeries("jblas(float)");
+		for (int i = min; i < max; i += bin) {
+			System.out.println("jblas(float) i=" + i);
+			org.jblas.FloatMatrix A = org.jblas.FloatMatrix.rand(i, i);
+			org.jblas.FloatMatrix b = org.jblas.FloatMatrix.rand(i);
+			long s = System.nanoTime();
+			org.jblas.Solve.solve(A, b);
+			long e = System.nanoTime();
+			data.add(i, (e - s) * 0.000001);
+		}
+		return data;
+	}
+	
+	XYSeries runEjml(){
 		// LinearSolverは細かくいろいろ設定できるみたい
 		// 行列演算の関数multなどが、出力を引数に与える形式で好みでない
-		XYSeries dataEJML = new XYSeries("ejml");
-		for(int i=2; i<n; i+=10){
-			System.out.println("ejml i="+i);
-			org.ejml.data.DenseMatrix64F A = RandomMatrices.createRandom(i, i,rand);;
-			org.ejml.data.DenseMatrix64F b = RandomMatrices.createRandom(i, 1, rand);
+		XYSeries data = new XYSeries("ejml");
+		long seed = System.currentTimeMillis();
+		Random rand = new Random(seed);
+		for (int i = min; i < max; i += bin) {
+			System.out.println("ejml i=" + i);
+			org.ejml.data.DenseMatrix64F A = RandomMatrices.createRandom(i, i,rand);
+			org.ejml.data.DenseMatrix64F b = RandomMatrices.createRandom(i, 1,rand);
 			org.ejml.data.DenseMatrix64F x = new org.ejml.data.DenseMatrix64F(i, 1);
 			long s = System.nanoTime();
 			LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.linear(i);
-			if( !solver.setA(A) ) {
+			if (!solver.setA(A)) {
 				System.out.println("Singular matrix");
 				System.exit(0);
 			}
-			if( solver.quality() <= 1e-8 ){
+			if (solver.quality() <= 1e-8) {
 				System.out.println("Nearly Singular matrix");
-//				System.exit(0);
+				// System.exit(0);
 			}
-			solver.solve(b,x);			
+			solver.solve(b, x);
 			long e = System.nanoTime();
-			dataEJML.add(i, (e - s)*0.000001);
+			data.add(i, (e - s) * 0.000001);
 		}
-		
-		// la4j
-		// float非対応
+		return data;
+	}
+	
+	XYSeries runLa4j(){
 		// Sparse対応
 		// なぜかGAUSSIANが一番早い
-		XYSeries dataLA4J = new XYSeries("la4j");
-		for(int i=2; i<n; i+=10){
-			System.out.println("la4j i="+i);
-			Matrix a = new Basic1DFactory().createRandomMatrix(i,i);
-			Vector b = new Basic1DFactory().createRandomVector(i);
+		XYSeries data = new XYSeries("la4j");
+		for (int i = min; i < max; i += bin) {
+			System.out.println("la4j i=" + i);
+			org.la4j.matrix.Matrix a = new Basic1DFactory().createRandomMatrix(
+					i, i);
+			org.la4j.vector.Vector b = new Basic1DFactory()
+					.createRandomVector(i);
 			long s = System.nanoTime();
 			LinearSystemSolver solver = a.withSolver(LinearAlgebra.GAUSSIAN);
 			solver.solve(b, LinearAlgebra.DENSE_FACTORY);
 			long e = System.nanoTime();
-			dataLA4J.add(i, (e - s)*0.000001);
+			data.add(i, (e - s) * 0.000001);
 		}
-		
-		// jama
-		// float非対応
-		XYSeries dataJAMA = new XYSeries("JAMA");
-		for(int i=2; i<n; i+=10){
-			System.out.println("jama i="+i);
+		return data;
+	}
+	
+	XYSeries runJama(){
+		XYSeries data = new XYSeries("JAMA");
+		for (int i = min; i < max; i += bin) {
+			System.out.println("jama i=" + i);
 			Jama.Matrix A = Jama.Matrix.random(i, i);
-			Jama.Matrix b = Jama.Matrix.random(i,1);
+			Jama.Matrix b = Jama.Matrix.random(i, 1);
 			long s = System.nanoTime();
 			A.solve(b);
 			long e = System.nanoTime();
-			dataJAMA.add(i, (e - s)*0.000001);
+			data.add(i, (e - s) * 0.000001);
 		}
-
-
-		
-		// JFreeChartによるグラフの作成
-		XYSeriesCollection data = new XYSeriesCollection();
-		data.addSeries(dataJblas);
-		data.addSeries(dataEJML);
-		data.addSeries(dataLA4J);
-		data.addSeries(dataJAMA);
-		JFreeChart chart = ChartFactory.createScatterPlot(
-				"Benchmark of linear solver", "Matrix size", "Time [ms]", data,
-				PlotOrientation.VERTICAL, true, false, false);
-
-		ChartPanel cpanel = new ChartPanel(chart);
-		getContentPane().add(cpanel, BorderLayout.CENTER);
+		return data;
+	}
+	
+	XYSeries runColt(){
+		XYSeries data = new XYSeries("colt");
+		cern.colt.matrix.DoubleFactory2D F = cern.colt.matrix.DoubleFactory2D.dense;
+		for (int i = min; i < max; i += bin) {
+			System.out.println("colt i=" + i);
+			cern.colt.matrix.DoubleMatrix2D A = F.random(i, i);
+			cern.colt.matrix.DoubleMatrix2D B = F.random(i, 1);
+			long s = System.nanoTime();
+			cern.colt.matrix.linalg.Algebra alg = new cern.colt.matrix.linalg.Algebra();
+			alg.solve(A, B);
+			long e = System.nanoTime();
+			data.add(i, (e - s) * 0.000001);
+		}
+		return data;
 	}
 }
