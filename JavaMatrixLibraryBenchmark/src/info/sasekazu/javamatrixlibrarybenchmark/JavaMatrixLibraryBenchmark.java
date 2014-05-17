93ca44a65c5d6476927761fffaa9be6f63d82ba7
Kazuya Sase <sasekazu@gmail.com>
2014-05-17 12:18:10 +0900

    Add la4j

--- JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
+++ JavaMatrixLibraryBenchmark/src/info/sasekazu/javamatrixlibrarybenchmark/JavaMatrixLibraryBenchmark.java
@@ -15,6 +15,11 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.la4j.LinearAlgebra;
+import org.la4j.factory.Basic1DFactory;
+import org.la4j.linear.LinearSystemSolver;
+import org.la4j.matrix.Matrix;
+import org.la4j.vector.Vector;
 
 public class JavaMatrixLibraryBenchmark extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -29,17 +34,17 @@ public class JavaMatrixLibraryBenchmark extends JFrame {
 
 	JavaMatrixLibraryBenchmark() {
 
-		final int n = 500;
+		final int n = 1000;
 		long seed = System.currentTimeMillis();
 		Random rand = new Random(seed);
-
+		
 		// jblas
 		XYSeries dataJblas = new XYSeries("jblas");
 		System.out.println("Matrix Solver Benchmark. Solve for X in AX = B");
-		for (int i = 2; i < n; i++) {
+		for (int i = 2; i < n; i+=10) {
 			System.out.println("jblas i="+i);
-			org.jblas.FloatMatrix A = org.jblas.FloatMatrix.rand(i, i);
-			org.jblas.FloatMatrix b = org.jblas.FloatMatrix.rand(i);
+			org.jblas.DoubleMatrix A = org.jblas.DoubleMatrix.rand(i, i);
+			org.jblas.DoubleMatrix b = org.jblas.DoubleMatrix.rand(i);
 			long s = System.nanoTime();
 			org.jblas.Solve.solve(A, b);
 			long e = System.nanoTime();
@@ -52,13 +57,13 @@ public class JavaMatrixLibraryBenchmark extends JFrame {
 		// LinearSolverは細かくいろいろ設定できるみたい
 		// 行列演算の関数multなどが、出力を引数に与える形式で好みでない
 		XYSeries dataEJML = new XYSeries("ejml");
-		for(int i=2; i<n; i++){
+		for(int i=2; i<n; i+=10){
 			System.out.println("ejml i="+i);
 			org.ejml.data.DenseMatrix64F A = RandomMatrices.createRandom(i, i,rand);;
 			org.ejml.data.DenseMatrix64F b = RandomMatrices.createRandom(i, 1, rand);
 			org.ejml.data.DenseMatrix64F x = new org.ejml.data.DenseMatrix64F(i, 1);
 			long s = System.nanoTime();
-			LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.leastSquares(i,i);
+			LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.linear(i);
 			if( !solver.setA(A) ) {
 				System.out.println("Singular matrix");
 				System.exit(0);
@@ -71,12 +76,29 @@ public class JavaMatrixLibraryBenchmark extends JFrame {
 			long e = System.nanoTime();
 			dataEJML.add(i, (e - s)*0.000001);
 		}
+		
+		// la4j
+		// float非対応
+		// Sparse対応
+		// なぜかGAUSSIANが一番早い
+		XYSeries dataLA4J = new XYSeries("la4j");
+		for(int i=2; i<n; i+=10){
+			System.out.println("la4j i="+i);
+			Matrix a = new Basic1DFactory().createRandomMatrix(i,i);
+			Vector b = new Basic1DFactory().createRandomVector(i);
+			long s = System.nanoTime();
+			LinearSystemSolver solver = a.withSolver(LinearAlgebra.GAUSSIAN);
+			solver.solve(b, LinearAlgebra.DENSE_FACTORY);
+			long e = System.nanoTime();
+			dataLA4J.add(i, (e - s)*0.000001);
+		}
 
 		
 		// JFreeChartによるグラフの作成
 		XYSeriesCollection data = new XYSeriesCollection();
 		data.addSeries(dataJblas);
 		data.addSeries(dataEJML);
+		data.addSeries(dataLA4J);
 		JFreeChart chart = ChartFactory.createScatterPlot(
 				"Benchmark of linear solver", "Matrix size", "Time [ms]", data,
 				PlotOrientation.VERTICAL, true, false, false);
